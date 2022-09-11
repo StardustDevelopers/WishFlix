ccc8c0e94ac2ba86cf88073347b83c9289bf86e6
wykz <wykzfivem@gmail.com>
2022-09-11 17:33:39 +0200

    Initial commit

---
+++ app/src/main/java/fr/stardustdevs/wishflix/HomeActivity.java
@@ -0,0 +1,14 @@
+package fr.stardustdevs.wishflix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
